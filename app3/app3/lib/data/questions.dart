c552080a16d4eaa933faf8880d2644c14422b39a
Jan Rybaczewski <janrybaczewski@gmail.com>
2024-11-20 17:44:48 +0100

    add data and model

---
+++ app3/app3/lib/data/questions.dart
@@ -0,0 +1,36 @@
+import 'package:app3/model/quiz_question.dart';
+//import 'package:app3/questions.dart';
+//import 'package:app3/quiz.dart';
+
+const questions = [
+  QuizQuestion([
+    "odpowiedz a",
+    "odpowiedz b",
+    "odpowiedz c",
+  ], "Pytanie 11"),
+  QuizQuestion([
+    "odpowiedz a",
+    "odpowiedz b",
+    "odpowiedz c",
+  ], "Pytanie 12"),
+  QuizQuestion([
+    "odpowiedz a",
+    "odpowiedz b",
+    "odpowiedz c",
+  ], "Pytanie 13"),
+  QuizQuestion([
+    "odpowiedz a",
+    "odpowiedz b",
+    "odpowiedz c",
+  ], "Pytanie 14"),
+  QuizQuestion([
+    "odpowiedz a",
+    "odpowiedz b",
+    "odpowiedz c",
+  ], "Pytanie 15"),
+  QuizQuestion([
+    "odpowiedz a",
+    "odpowiedz b",
+    "odpowiedz c",
+  ], "Pytanie 16"),
+];
---
+++ app3/app3/lib/model/quiz_question.dart
@@ -0,0 +1,5 @@
+class QuizQuestion {
+  final String quesiton;
+  final List<String> asnwers;
+  const QuizQuestion(this.asnwers, this.quesiton);
+}
